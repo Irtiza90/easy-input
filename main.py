8e05ddef5fb3b9a263565883eae8d613133ed0a7
Irtiza Babar <61447963+Irtiza90@users.noreply.github.com>
2022-06-08 13:56:19 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,49 @@
 import re
 
 
-def ask_int(prompt: str, message=...) -> int:
+def ask_int(prompt: str, message: str = None) -> int:
+    """
+    Asks an integer
+
+    :param prompt: the prompt to show when asking input
+    :param message: the message that will be printed if input is invalid
+    :returns: int
+    """
     while True:
         try:
             inp = int(input(prompt))
-        except TypeError:
+        except ValueError:
             print("Expected a number" if not message else message)
         else:
             return inp
 
 
-def ask_float(prompt: str, message=...) -> float:
+def ask_float(prompt: str, message: str = None) -> float:
+    """
+    Asks an Float
+
+    :param prompt: the prompt to show when asking input
+    :param message: the message that will be printed if input is invalid
+    :returns: float
+    """
     while True:
         try:
             inp = float(input(prompt))
-        except TypeError:
+        except ValueError:
             print("Expected a floating point number" if not message else message)
         else:
             return inp
 
 
-def ask_bool(prompt: str, message=...) -> bool:
+def ask_bool(prompt: str, message: str = None) -> bool:
+    """
+    Asks an Boolean
+
+    :param prompt: the prompt to show when asking input
+    :param message: the message that will be printed if input is invalid
+    :returns: bool
+    """
+
     bools = {
         "true": True,
         "True": True,
@@ -38,12 +60,19 @@ def ask_bool(prompt: str, message=...) -> bool:
         print("Expected boolean" if not message else message)
 
 
-def ask_regex(prompt: str, pattern: str | re.Pattern, message=...) -> str:
+def ask_regex(prompt: str, pattern: str | re.Pattern, message: str = None) -> str:
+    """
+    Will Keep asking for input until it matches regex
+
+    :param prompt: the prompt to show when asking input
+    :param pattern: The regex pattern that will be matched
+    :param message: the message that will be printed if input is invalid
+    :returns: the input string that matched the regex pattern
+    """
     while True:
         inp = input(prompt)
 
         if re.match(pattern=pattern, string=inp):
             return inp
 
-        print("Expected boolean" if not message else message)
-
+        print("Invalid Input" if not message else message)
